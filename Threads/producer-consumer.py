4b9601eab2727f5c699b8b3ace458d59e313e5ef
Sebastian <sebastian.varv@gmail.com>
2017-11-01 11:31:43 +0200

    Terminating consumer thread

--- Threads/producer-consumer.py
+++ Threads/producer-consumer.py
@@ -4,10 +4,6 @@ import threading
 from argparse import ArgumentParser
 from collections import Counter
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s (%(threadName)-2s) %(message)s',
-                    )
-
 
 class Queue:
     def __init__(self):
@@ -16,19 +12,15 @@ class Queue:
 
     def put(self, e):
         with self.__cv:
-            logging.debug('Added element')
             self.__q.append(e)
             if len(self.__q) == 1:
                 # First element added - notify all waiting threads
-                logging.debug('Queue is no more empty, notifying all' \
-                              'waiting threads')
                 self.__cv.notifyAll()
 
     def get(self):
         e = None
         with self.__cv:
             if len(self.__q) <= 0:
-                logging.debug('Queue is empty, waiting ...')
                 self.__cv.wait()
             e = self.__q.pop()
         return e
@@ -39,26 +31,25 @@ def remove_punct(s):
     return s.translate(table, string.punctuation)
 
 
-def consumer(q, count):
+def consumer(q, count, files):
     """wait for the condition and use the resource"""
-    logging.debug('Starting consumer thread')
-    while True:
-        wd = q.get()
-        logging.debug('consumed: %s' % wd)
-        count[wd] += 1
+
+    for i in range(len(files)):
+        words = q.get()
+        for wd in words:
+            count[wd] += 1
 
 
 def producer(q, files):
     """set up the resource to be used by the consumer"""
-    logging.debug('Starting producer thread')
     for file in files:
         with open(file) as f:
+            words = []
             for line in f:
                 parts = line.split()
                 for word in parts:
-                    wd = remove_punct(word.strip())
-                    q.put(wd)
-                    logging.debug('Added: %s' % wd)
+                    words.append(remove_punct(word.strip()))
+            q.put(words)
                     # time.sleep(1)
 
 
@@ -76,13 +67,12 @@ if __name__ == '__main__':
     count = Counter()
 
     q = Queue()
-    cons = threading.Thread(name='Consumer', target=consumer, args=(q, count))
-    prod = threading.Thread(name='Producer', target=producer, args=(q, files.split()))
+    files_list = files.split()
+    cons = threading.Thread(name='Consumer', target=consumer, args=(q, count, files_list))
+    prod = threading.Thread(name='Producer', target=producer, args=(q, files_list))
 
     cons.start()
     prod.start()
 
     cons.join()
     prod.join()
-
-    print(list(count.elements()))
