39321c87e6432f834365303c7def77f9355b44ef
Sebastian <sebastian.varv@gmail.com>
2017-11-08 23:58:19 +0200

    Common file for protocols needed to communicate with server

---
+++ Multiplayer-Sudoku/common.py
@@ -0,0 +1,44 @@
+# Requests --------------------------------------------------------------------
+# User enters nick name, app checks if it is valid and not taken
+__REQ_REG_USER = '1'
+# Connect to server with port number
+__REQ_CONNECT_SERVER_PORT = '2'
+# Return existing sessions of games (id, number of players, max players)
+__REQ_GET_GAMES = '3'
+# Set max players num, returns session id, adds client to game
+__REQ_CREATE_GAME = '4'
+# Send Player id, game room id to add player request
+__REQ_ADD_PLYER_TO_GAMEROOM = '5'
+
+# Send req with game_id, (x,y) coord and val
+__REQ_MAKE_MOVE = '6'
+# Get board and current scores together, if game is over then return winner
+__REQ_GET_GAME = '7'
+
+__CTR_MSGS = {__REQ_REG_USER: 'Register user with nickname',
+              __REQ_CONNECT_SERVER_PORT: 'Connect game server port',
+              __REQ_GET_GAMES: 'Get all game sessions (game rooms)',
+              __REQ_CREATE_GAME: 'Create new game room',
+              __REQ_ADD_PLYER_TO_GAMEROOM: 'Add player to game room',
+              __REQ_MAKE_MOVE: 'Player make move',
+              __REQ_GET_GAME: 'Return current game state with board and scores'
+              }
+# Responses--------------------------------------------------------------------
+__RSP_OK = '0'
+# Responses for connecting server with port number
+__RSP_CONNECTION_REFUSED = '1'
+__RSP_SERVER_NOT_FOUND = '2'
+# For all else problems beside server not found etc.
+__RSP_CONNECTION_ERROR = '3'
+# User tries to join game room which is full
+__RSP_GAME_FULL_ERROR = '4'
+
+__ERR_MSGS = {__RSP_OK: 'No Error',
+              __RSP_CONNECTION_REFUSED: 'Connection refused',
+              __RSP_SERVER_NOT_FOUND: 'Server not found',
+              __RSP_CONNECTION_ERROR: 'Connection error',
+              __RSP_GAME_FULL_ERROR: 'Game room is full'
+              }
+# Field separator for sending multiple values ---------------------------------
+__MSG_FIELD_SEP = ':'
+__MSG_END = '__END'
--- Multiplayer-Sudoku/readme.txt
+++ Multiplayer-Sudoku/readme.txt
@@ -3,4 +3,4 @@
 
 Multiplayer sudoku where player can create and joing gamerooms and play
 competetive sudoku where player is awarded a point when correct number is
-inserted. Player witht the most points wins.
+inserted. Player with most points wins.
