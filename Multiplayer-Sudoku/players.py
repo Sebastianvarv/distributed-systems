afad7fb906a969b7291238646375bd449a78e281
jannopet <janno.peterson@gmail.com>
2017-11-09 21:15:51 +0200

    Added get player name method

--- Multiplayer-Sudoku/players.py
+++ Multiplayer-Sudoku/players.py
@@ -9,5 +9,8 @@ class Players:
     def reg_player(self, name):
         player_id = uuid.uuid4()
         self.players[player_id] = name
+        return player_id
 
+    def get_player_name(self, player_id):
+        return self.players[player_id]
     
