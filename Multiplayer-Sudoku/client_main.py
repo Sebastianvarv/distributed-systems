5f6fb02fef98193d7332f27490de8969dc332322
Martin Liivak <martin.liivak@gmail.com>
2017-11-09 22:04:12 +0200

    Added lobby GUI with some backend shizz.

--- Multiplayer-Sudoku/client_main.py
+++ Multiplayer-Sudoku/client_main.py
@@ -1,15 +1,28 @@
-from clientpornoo import main
+from Tkinter import Tk
+from client_input import input_main, initiate_lobby, update_lobby, destroy_lobby_window
 from client import *
+import time
 
 if __name__ == "__main__":
-    port, nickname = main()
+    root = Tk()
+    port, nickname = input_main(root)
     print(port, nickname)
 
-    sock = connect_server(port)
-    reg_user(sock, nickname)
+    # Dummy shit
+    #sock = connect_server(port)
+    #reg_user(sock, nickname)
 
-    games = req_get_games(sock)
-
+    initiate_lobby(root)
+    #games = req_get_games(sock)
     # (room_id, num players, max players)
     games = [(1, 3, 4), (2, 1, 5), (3, 3, 3)]
-    print(games)
+    # \Dummy shit
+
+    # This update needs to be polled!
+    update_lobby(root, games)
+    t1 = time.time()
+    while True:
+        if time.time() - t1 > 5:
+            destroy_lobby_window(root)
+            break
+
