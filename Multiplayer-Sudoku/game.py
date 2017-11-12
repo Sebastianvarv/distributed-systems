067241f7afcec80b7d510ef3d1e09f13cd288693
jannopet <janno.peterson@gmail.com>
2017-11-12 22:28:48 +0200

    max players and some other stuff

--- Multiplayer-Sudoku/game.py
+++ Multiplayer-Sudoku/game.py
@@ -1,24 +1,23 @@
 
 import uuid
 
-
 def read_solution():
     f = open("solutions.txt", "r")
-    solution = [[None] * 9 for x in range(0, 9)]
+    solution = [[None]*9 for x in range(0,9)]
     for i in range(9):
         row = f.readline().strip()
         split_row = row.split(" ")
         for j in range(9):
-            solution[i][j] = int(split_row[j])
-
+            solution[i][j] = int (split_row[j])
+                     
     f.readline()
-
-    board = [[None] * 9 for x in range(0, 9)]
+    
+    board = [[None]*9 for x in range(0,9)]
     for i in range(9):
         row = f.readline().strip()
         split_row = row.split(" ")
         for j in range(9):
-            board[i][j] = int(split_row[j])
+            board[i][j] = int (split_row[j])
 
     f.close()
 
@@ -27,24 +26,24 @@ def read_solution():
 
 class Game:
 
-    def __init__(self, host_nickname):
+    def __init__(self, max_players):
         self.solution, self.board = read_solution()
-        self.names = {}
         self.scores = {}
-        self.add_player(host_nickname)
+        self.max_players = max_players
+        self.game_started = False
 
     def make_move(self, user_id, (x,y), value):
-        if self.valid_move((x, y), value):
+        if valid_move((x,y), value):
             self.scores[user_id] += 1
             return 1
         self.scores[user_id] -= 1
         return 0
             
     def valid_move(self, (x,y), value):
-        if self.solution[x][y] == value:
+        if solution[x][y] == value:
             return True
         return False
-
+            
     def check_game_won(self):
         for i in range(9):
             for j in range(9):
@@ -54,3 +53,8 @@ class Game:
 
     def add_player(self, player_id):
         self.scores[player_id] = 0
+        if len(self.scores) == self.max_players:
+            self.game_started = True
+
+    def get_num_players(self):
+        return len(self.scores)
