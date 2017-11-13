ddc8ed48149731416c8594bac5d4cdbf6c0317f2
Markus Loide <markusloide@gmail.com>
2017-11-13 13:21:48 +0200

    Threaded lobby updating

--- Multiplayer-Sudoku/client_main.py
+++ Multiplayer-Sudoku/client_main.py
@@ -2,6 +2,24 @@ from Tkinter import Tk
 from client_input import input_main, initiate_lobby, update_lobby, destroy_lobby_window
 from client import *
 import time
+import threading
+
+lobby_update_thread = None
+lobby_data = None
+
+
+def updatelob(root, port):
+    global lobby_data
+    games = req_get_games(port)
+    lobby_data = update_lobby(root, games)
+    print "lobby updating returned:", lobby_data
+
+    if lobby_data is not None:
+        pass
+    else:
+        time.sleep(0.1)
+        updatelob(root, port)
+
 
 if __name__ == "__main__":
     root = Tk()
@@ -9,23 +27,18 @@ if __name__ == "__main__":
     print(port, nickname)
 
     # Dummy shit
-    player_id = reg_user(nickname, port)
+    user_id = reg_user(nickname, port)
 
     initiate_lobby(root)
-    # (room_id, num players, max players)
-    game_id = req_create_game(player_id, 4, port)
-    games = req_get_games(port)
+
+    lobby_update_thread = threading.Thread(target=updatelob(root, port))
+    lobby_update_thread.start()
+
+    print lobby_data
+
+    # TODO: Do the request based on the information, i.e. create game or join game
+    print("Doing create game or join game request")
+
     # (room_id, num players, max players)
     # games = [(1, 3, 4), (2, 1, 5), (3, 3, 3)]
     # \Dummy shit
-
-    print "games: " + str(games)
-
-    # This update needs to be polled!
-    update_lobby(root, games)
-    t1 = time.time()
-    while True:
-        if time.time() - t1 > 5:
-            destroy_lobby_window(root)
-            break
-
